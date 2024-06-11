3ea4f42a0f0f3c5f670e950178d34b4583ab3433
airlorm <elormnizi@gmail.com>
2024-06-11 11:28:12 +0300

    added calculated fields to the result component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,38 @@
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
 export default function Results({ userInput }) {
-  console.log(userInput);
-  return <p>Result</p>;
+  console.log(calculateInvestmentResults(userInput));
+  let results = calculateInvestmentResults(userInput);
+  return (
+    <div>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Interest</th>
+            <th>Value at End of Year</th>
+            <th>Annual Investment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((result, index) => (
+            <tr key={index}>
+              <td>{isNaN(result.year) ? "N/A" : result.year}</td>
+              <td>{isNaN(result.interest) ? "N/A" : result.interest}</td>
+              <td>
+                {isNaN(result.valueEndOfYear)
+                  ? "N/A"
+                  : formatter.format(result.valueEndOfYear)}
+              </td>
+              <td>
+                {isNaN(result.annualInvestment)
+                  ? "N/A"
+                  : result.annualInvestment}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
